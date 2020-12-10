c51f41ff755d17ac8b5a2bb2799961a9427ed86f
Nicolas Walcker de Miranda <walcker_@outlook.com>
2020-12-09 22:14:59 -0300

    Initial commit

---
+++ js/countdown.js
@@ -0,0 +1,31 @@
+
+// Definir a data que vai entrar em contagem regressiva
+var countDownDate = new Date("April 25, 2022 12:00:00").getTime();
+
+// Atualizando a contagem regressiva a cada 1 segundo
+var x = setInterval(function () {
+
+    //Obtendo a data e hora de hoje
+    var now = new Date().getTime();
+
+    // Encontrando a distância de agora e a data da contagem
+    var distance = countDownDate - now;
+
+    // Cálculo do tempo e dias, horas, minutos e segundos
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Exibir o resultado no elemento com id selecionado
+    document.getElementById("dday").innerHTML = days + " : " + hours + " : " +
+    minutes + " : " + seconds + "  ";
+
+    // Quando o contador chegar a 0, exiba a mensagem ou faça algo 
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("dday").innerHTML = "O GRANDE DIA CHEGOU";
+    }
+}, 1000);
+
+console.log(countDownDate)
